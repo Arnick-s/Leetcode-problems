2463c72933bafa2ae4f91100914b492f05c620a6
Arnick-s <96995811+Arnick-s@users.noreply.github.com>
2022-10-10 00:54:55 +0530

    Time: 19 ms (97.12%), Space: 13.1 MB (82.34%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        set<vector<int>> ans;
+        vector<vector<int>> total;
         int n = nums.size();
-        if(n<4)return {};
-        for(int first = 0;first<n-3;first++){
-            for(int forth = 3;forth<n;forth++){
-                int second = first+1;
-                int third = forth - 1;
-                while(second<third){
-                    long long sum = (long long)nums[first]+(long long)nums[second]+(long long)nums[third]+(long long)nums[forth];
-                    if(sum == target)
-                    {
-                        ans.insert({nums[first],nums[second++],nums[third],nums[forth]});
-                    }
-                    else if(sum<target){
-                        second++;
-                    }
-                    else {
-                        third--;
+        if(n<4)  return total;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-3;i++)
+        {
+            if(i>0&&nums[i]==nums[i-1]) continue;
+            if((long long)nums[i]+(long long)nums[i+1]+(long long)nums[i+2]+(long long)nums[i+3]>target) break;
+            if((long long)nums[i]+(long long)nums[n-3]+(long long)nums[n-2]+(long long)nums[n-1]<target) continue;
+            for(int j=i+1;j<n-2;j++)
+            {
+                if(j>i+1&&nums[j]==nums[j-1]) continue;
+                if((long long)nums[i]+(long long)nums[j]+(long long)nums[j+1]+(long long)nums[j+2]>target) break;
+                if((long long)nums[i]+(long long)nums[j]+(long long)nums[n-2]+(long long)nums[n-1]<target) continue;
+                int left=j+1,right=n-1;
+                while(left<right){
+                    int sum=nums[left]+nums[right]+nums[i]+nums[j];
+                    if(sum<target) left++;
+                    else if(sum>target) right--;
+                    else{
+                        total.push_back(vector<int>{nums[i],nums[j],nums[left],nums[right]});
+                        do{left++;}while(nums[left]==nums[left-1]&&left<right);
+                        do{right--;}while(nums[right]==nums[right+1]&&left<right);
                     }
                 }
             }
         }
-        vector<vector<int>> res;
-        for(auto it:ans){
-            res.push_back(it);
-        }
-        return res;
+        return total;
     }
 };
