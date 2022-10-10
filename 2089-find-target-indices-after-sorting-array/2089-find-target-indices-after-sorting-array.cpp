8968439cdb6f0071f1af7199df9ea2bee26bf098
Arnick-s <96995811+Arnick-s@users.noreply.github.com>
2022-10-10 16:52:09 +0530

    Time: 7 ms (78.27%), Space: 11.7 MB (15.20%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,40 +1,13 @@
 class Solution {
 public:
-    vector<int> targetIndices(vector<int>& nums, int target) {
-        
-        sort(nums.begin(),nums.end());
-        nums.push_back(7);
-        int n = nums.size();
-        int l = -1;
-        int r = n-1;
-        
-        while(r-l>1){
-            int mid = l + (r-l)/2;
-            
-            if(nums[mid]>=target){
-                r = mid;
-            }
-            else if(nums[mid]<target){
-                l = mid;
-            }
+    vector<int> targetIndices(vector<int>& A, int target) {
+        int cnt = 0, rank = 0; // `cnt` is the frequency of `target`, `rank` is the sum of frequencies of all numbers < target
+        for (int n : A) {
+            cnt += n == target;
+            rank += n < target;
         }
-        int leftIdx = l;
-        l = -1;
-        r = n-1;
-        
-        while(r-l>1){
-            int mid = l + (r-l)/2;
-            
-            if(nums[mid]<=target)
-                l = mid;
-            else r = mid;
-        }
-        int rightIdx = r;
-        vector<int> res(rightIdx - leftIdx - 1);
-        int x = 0;
-        while(++leftIdx<rightIdx){
-            res[x++] = leftIdx;
-        }
-        return res;
+        vector<int> ans;
+        while (cnt--) ans.push_back(rank++);
+        return ans;
     }
 };
