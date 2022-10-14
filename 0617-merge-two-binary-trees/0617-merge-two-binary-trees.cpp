5b8873dc4cf932f7468b78058a3af0fc78997e86
Arnick-s <96995811+Arnick-s@users.noreply.github.com>
2022-10-15 00:27:09 +0530

    Time: 28 ms (99.54%), Space: 32.3 MB (59.74%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -12,15 +12,30 @@
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        if(!root1 or !root2){
-            if(root1 == NULL and root2 == nullptr) return NULL;
-            if(root1 == NULL)return root2;
-            if(root2 == nullptr)return root1;
+        if(!root1 || !root2){
+            return root1?:root2;
         }
-        
+        if(root1->left == NULL || root1->right == NULL){
+            if(!(root1->left or root1->right)){
+                root1->val += root2->val;
+                root1->left = root2->left;
+                root1->right = root2->right;
+                return root1;
+            }else if(!root1->left){
+                root1->val += root2->val;
+                root1->left = root2->left;
+                mergeTrees(root1->right,root2->right);
+                return root1;
+            }else{
+                root1->val += root2->val;
+                root1->right = root2->right;
+                mergeTrees(root1->left,root2->left);
+                return root1;
+            }
+        }        
         root1->val  += root2->val;
-        root1->left  = mergeTrees(root1->left,root2->left);
-        root1->right = mergeTrees(root1->right,root2->right);
+        mergeTrees(root1->left,root2->left);
+        mergeTrees(root1->right,root2->right);
         return root1;
     }
 };
