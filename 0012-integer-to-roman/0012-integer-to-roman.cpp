1b178b37400313e6a9b26c189c93e6cf38836536
Arnick-s <96995811+Arnick-s@users.noreply.github.com>
2022-10-15 02:33:33 +0530

    Time: 14 ms (58.01%), Space: 5.9 MB (77.71%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,47 +1,11 @@
 class Solution {
 public:
-    string intToRoman(int nums) {
-        map<int,string>hash;
-        hash[1] = "I";
-        hash[4] = "IV";
-        hash[5] = "V";
-        hash[9] = "IX";
-        hash[10] = "X";
-        hash[40] = "XL";
-        hash[50] = "L";
-        hash[90] = "XC";
-        hash[100] = "C";
-        hash[400] = "CD";
-        hash[500] = "D";
-        hash[900] = "CM";
-        hash[1000] = "M";
+    string intToRoman(int num) {
+        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string ths[]={"","M","MM","MMM"};
         
-        string res = "";
-        int rem = 0;
-        int place = 1;
-        while(nums>0){
-            if(rem == 0){
-                rem = nums%10 * place;
-                place*=10;
-                nums/=10;
-                string temp = "";
-                while(rem>0){
-                    auto it = hash.upper_bound(rem);
-                    it--;
-                    temp += it->second;
-                    rem -= it->first;
-                }
-                res = temp + res;
-            }
-            
-        }
-        return res;
+        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
     }
 };
-        // hash['I' - 'A'] = 1;
-        // hash['V' - 'A'] = 5;
-        // hash['X' - 'A'] = 10;
-        // hash['L' - 'A'] = 50;
-        // hash['C' - 'A'] = 100;
-        // hash['D' - 'A'] = 500;
-        // hash['M' - 'M'] = 1000;
