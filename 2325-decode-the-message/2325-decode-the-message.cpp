32aa7c39e618b7124aed9413b6db5a576c3e6507
Arnick-s <96995811+Arnick-s@users.noreply.github.com>
2022-10-16 02:19:31 +0530

    Time: 3 ms (92.14%), Space: 6.9 MB (73.19%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string decodeMessage(string s, string m) {
+        int n = s.size();
+        vector<char>KEY(26);
+        vector<bool>seen(26,false);
+        
+        int x = 0;
+        for(int i = 0;i<n;i++){
+            if(s[i] != ' '){
+                if(seen[s[i]-'a'] == false)
+                {
+                    KEY[s[i]-'a'] = x + 'a';
+                    x++;
+                    seen[s[i]-'a'] = true;
+                }
+            }
+        }
+        
+        for(int i = 0;i<m.size();i++){
+            if(m[i]!=' '){
+                m[i] = KEY[m[i]-'a'];
+            }
+        }
+        return m;
+    }
+};
