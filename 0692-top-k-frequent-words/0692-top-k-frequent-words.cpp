e64dedde3b6ad589101616c53ca5ed97d00cb4b4
Arnick-s <96995811+Arnick-s@users.noreply.github.com>
2022-10-19 06:01:57 +0530

    Time: 25 ms (52.25%), Space: 12.3 MB (98.46%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string,int> hash;
+        
+        for(int i = 0;i<words.size();i++){
+            hash[words[i]]++;
+        }
+        vector<string> res(k);
+        for(int i = 0;i<k;i++){
+            string maxS;
+            auto maxFreqIt = hash.begin();
+            for(auto it = hash.begin();it != hash.end();it++){
+                if(it->second > maxFreqIt->second){
+                    maxS = it->first;
+                    maxFreqIt = it;
+                }else if(it->second == maxFreqIt->second && it->first < maxFreqIt->first){
+                    maxS = it->first;
+                    maxFreqIt = it;
+                }
+            }
+            cout<<maxFreqIt->first<<endl;
+            maxFreqIt->second = 0;
+            res[i] = maxFreqIt->first;
+        }
+        return res;
+    }
+};
