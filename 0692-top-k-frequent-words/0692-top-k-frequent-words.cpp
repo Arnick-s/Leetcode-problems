4a281266e4ae8df32107313d46231221813161cd
Arnick-s <96995811+Arnick-s@users.noreply.github.com>
2022-10-19 06:04:34 +0530

    Time: 12 ms (92.78%), Space: 12.2 MB (99.46%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -8,20 +8,16 @@ public:
         }
         vector<string> res(k);
         for(int i = 0;i<k;i++){
-            string maxS = hash.begin()->first;
             auto maxFreqIt = hash.begin();
             for(auto it = hash.begin();it != hash.end();it++){
                 if(it->second > maxFreqIt->second){
-                    maxS = it->first;
                     maxFreqIt = it;
                 }else if(it->second == maxFreqIt->second && it->first < maxFreqIt->first){
-                    maxS = it->first;
                     maxFreqIt = it;
                 }
             }
-            cout<<maxFreqIt->first<<endl;
             maxFreqIt->second = 0;
-            res[i] = maxS;
+            res[i] = maxFreqIt->first;
         }
         return res;
     }
