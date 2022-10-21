92500ef309cf11247736d4156e896984086abfe2
Arnick-s <96995811+Arnick-s@users.noreply.github.com>
2022-10-22 02:46:18 +0530

    Time: 3 ms (48.01%), Space: 6.6 MB (53.18%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
+    const vector<string> pad = {
+        "", "", "abc", "def", "ghi", "jkl",
+        "mno", "pqrs", "tuv", "wxyz"
+    };
+    
     vector<string> letterCombinations(string digits) {
-        if(digits.length() == 0)return {};
-        vector<string> keypad(10,"");
-        keypad = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        int n = digits.length();
-        vector<string> ans;
-        for(int i = 0;n>=1 && i<keypad[digits[0]-'0'].length();i++){
-            string temp = "";
-            temp += keypad[digits[0]-'0'][i];
-            for(int j = 0;n>=2 && j<keypad[digits[1]-'0'].length();j++){
-                string temp1 = temp + keypad[digits[1]-'0'][j];
-                for(int k = 0;n>=3 && k<keypad[digits[2]-'0'].length();k++){
-                    string temp2 = temp1 + keypad[digits[2]-'0'][k];
-                    for(int l = 0;n>=4 && l<keypad[digits[3]-'0'].length();l++){
-                        ans.push_back(temp2 + keypad[digits[3]-'0'][l]);
-                    }
-                    if(n<4){
-                        ans.push_back(temp2);
-                    }
+        if (digits.empty()) return {};
+		vector<string> result;
+        result.push_back("");
+        
+        for(auto digit: digits) {
+            vector<string> tmp;
+            for(auto candidate: pad[digit - '0']) {
+                for(auto s: result) {
+                    tmp.push_back(s + candidate);
                 }
-                if(n<3)
-                    ans.push_back(temp1);
             }
-            if(n<2)
-                ans.push_back(temp);
+            result.swap(tmp);
         }
-        return ans;
+        return result;
     }
 };
