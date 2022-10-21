f969a44d924fb2f3777419895e46edfcd59dfe0f
Arnick-s <96995811+Arnick-s@users.noreply.github.com>
2022-10-22 02:47:49 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (53.18%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    const vector<string> pad = {
-        "", "", "abc", "def", "ghi", "jkl",
-        "mno", "pqrs", "tuv", "wxyz"
-    };
-    
-    vector<string> letterCombinations(string digits) {
-        if (digits.empty()) return {};
-		vector<string> result;
-        result.push_back("");
-        
-        for(auto digit: digits) {
-            vector<string> tmp;
-            for(auto candidate: pad[digit - '0']) {
-                for(auto s: result) {
-                    tmp.push_back(s + candidate);
-                }
-            }
-            result.swap(tmp);
-        }
-        return result;
+    const vector<string> pad = {"", "", "abc", "def", "ghi", "jkl","mno", "pqrs", "tuv", "wxyz"};
+    vector<string> letterCombinations(string digit) {
+        if(digit.length()==0)return {};
+        vector<string>ans;
+        f(0,digit,ans,""); 
+        return ans;
+    }
+    void f(int idx,string &digit,vector<string>&ans,string s){
+        if(idx==digit.length())ans.push_back(s);
+        else for(auto c : pad[digit[idx]-'0'])f(idx+1,digit,ans,s+c); 
     }
 };
