103242b76c3351e03f04aaee54c8e27fcf3d9308
Arnick-s <96995811+Arnick-s@users.noreply.github.com>
2022-10-21 11:51:14 +0530

    Time: 446 ms (24.83%), Space: 77.1 MB (60.05%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -5,15 +5,14 @@ public:
     unordered_map<int,int> nmap;
     for (int i = 0; i <nums.size();i++)
     {
-        if (nmap.count(nums[i]) == 0)
-            nmap[nums[i]] = i;
-        else if (i - nmap[nums[i]] <=k)
-            return true;
-        else
-            nmap[nums[i]] = i;
+        
+        if(nmap.count(nums[i]) == 1){
+            if(i - nmap[nums[i]] <= k)
+                return true;
+        }
+        nmap[nums[i]] = i;
     }
     
-    return false;
-    
-}
+    return false; 
+    }
 };
