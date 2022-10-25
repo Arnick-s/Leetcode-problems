7463e6aef4a3325f8b850dd66995429a1dd13563
Arnick-s <96995811+Arnick-s@users.noreply.github.com>
2022-10-26 02:24:29 +0530

    Time: 27 ms (61.19%), Space: 11.2 MB (59.27%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -3,11 +3,11 @@ public:
     int diagonalSum(vector<vector<int>>& mat) {
         int n = mat.size();
         int sum = 0;
-        for(int i = 0;i<n;i++){
-            sum += (mat[i][i] + mat[i][n-1 - i]);
+        for(int i = 0;i<n/2;i++){
+            sum += (mat[i][i] + mat[i][n-1 - i] + mat[n-1 - i][i] + mat[n-1 -i][n-1- i]);
         }
         
-        if(n%2 == 1) sum -= mat[n/2][n/2];
+        if(n%2 == 1) sum += mat[n/2][n/2];
         return sum;
     }
 };
