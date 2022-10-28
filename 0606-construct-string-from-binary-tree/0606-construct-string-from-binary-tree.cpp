8672c3e01ca8d9e347e315d0f2480e6e42fe4d16
Arnick-s <96995811+Arnick-s@users.noreply.github.com>
2022-10-29 01:28:45 +0530

    Time: 23 ms (92.53%), Space: 53.7 MB (32.61%) - LeetHub

--- 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.cpp
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
-    static string tree2str(const TreeNode* root) {
-        string ans;
-        tree2Str(root, ans);
-        return ans;
-    }
-    static void tree2Str(const TreeNode* node, string& ans) {
-        if (!node) return;   
-        ans += to_string(node->val);
-        if (node->left) { //left side check
-            ans += '(';
-            tree2Str(node->left, ans);
-            ans += ')';
-        }
-        if (node->right) { //right side check
-            if (!node->left) ans += "()"; //left side not present, but right side present
-            ans += '(';
-            tree2Str(node->right, ans);
-            ans += ')';
-        }
+   string tree2str(TreeNode* t) {
+        if (t == NULL) return "";
+        string s = to_string(t->val);
+        if (t->left) s += '(' + tree2str(t->left) + ')';
+        else if (t->right) s += "()";
+        if (t->right) s += '(' + tree2str(t->right) + ')';
+        return s;
+    
     }
 };
