67de592fdb8b1d339c9dbdc3cd8d6b6e5942e41e
Arnick-s <96995811+Arnick-s@users.noreply.github.com>
2022-10-28 23:03:20 +0530

    Time: 7 ms (99.33%), Space: 13 MB (67.34%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
-        unordered_map<int,vector<int>> mp;
-        int n = groupSizes.size();
+        map<int,vector<int>> group;
         vector<vector<int>> res;
         
-        for(int i = 0;i<n;i++){
-            if(mp.find(groupSizes[i]) == mp.end()){
-                mp[groupSizes[i]] = {i};
-            }else{
-                mp[groupSizes[i]].push_back(i);
-            }
-            
-            if(mp.find(groupSizes[i])->second.size() == groupSizes[i]){
-                res.push_back(mp[groupSizes[i]]);
-                mp.erase(groupSizes[i]);
+        for(int i = 0;i < groupSizes.size(); ++i){
+            group[groupSizes[i]].push_back(i);
+        }
+        for(auto & it : group){
+            for(auto curr = it.second.begin(); curr != it.second.end(); curr += it.first){
+                res.push_back(vector<int>(curr,curr+it.first));
             }
         }
-        for(auto &it: mp){
-            res.push_back(it.second);
-        }
-         return res;   
+        return res;
     }
 };
