8bb63fd73aa05ca958aae2aac52d469383ca3280
Arnick-s <96995811+Arnick-s@users.noreply.github.com>
2022-10-31 11:17:19 +0530

    Time: 250 ms (16.59%), Space: 49.9 MB (11.60%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
@@ -7,12 +7,11 @@ public:
     
     vector<TreeNode*> helper(int n){
         
-        vector<TreeNode*> res;
         if(n == 1){
-            TreeNode* root = new TreeNode(0);
-            res.push_back(root) ;
-            return res;
+            return {new TreeNode(0)};
         }
+        vector<TreeNode*> res;
+        
         for(int i = 1;i<n;i+=2){
             vector<TreeNode*> left = helper(i);
             vector<TreeNode*> right = helper(n-1-i);
