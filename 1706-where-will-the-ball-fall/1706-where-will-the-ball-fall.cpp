4bec961e3d377d4994b4d777c9376fb03a456a6c
Arnick-s <96995811+Arnick-s@users.noreply.github.com>
2022-11-01 17:28:47 +0530

    Time: 66 ms (21.09%), Space: 13.1 MB (80.92%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
-     vector<int> findBall(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector<int> res;
-        for (int i = 0; i < n; ++i) {
-            int i1 = i, i2;
-            for (int j = 0; j < m; ++j) {
-                i2 = i1 + grid[j][i1];
-                if (i2 < 0 || i2 >= n || grid[j][i2] != grid[j][i1]) {
-                    i1 = -1;
-                    break;
+    vector<int> findBall(vector<vector<int>>& grid) {
+        //+1 -> look at right
+        //-1 -> look at left
+        
+        int m = grid[0].size();
+        int n = grid.size();
+        vector<int> res(m,-1);
+        
+        for(int jj = 0;jj<m;jj++){
+            int j = jj;
+            bool ok = true;
+            for(int i = 0;i<n;i++){
+                if(grid[i][j] == +1){
+                    if(j == m-1||grid[i][j+1] == -1){
+                        ok = false;
+                        break;
+                    }
+                    j++;
                 }
-                i1 = i2;
+                else{
+                    if(j == 0 || grid[i][j-1] == 1){
+                        ok = false;
+                        break;
+                    }
+                    j--;
+                }
             }
-            res.push_back(i1);
+            if(ok)
+                res[jj] = j ;
         }
         return res;
     }
