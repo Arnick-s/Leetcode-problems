c558d563f4eb66bc3437c19e1390619cdb3b9353
Arnick-s <96995811+Arnick-s@users.noreply.github.com>
2022-11-08 08:36:34 +0530

    Time: 5 ms (54.29%), Space: 6.6 MB (43.30%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    string makeGood(string s) {
+        stack<char> st;
+        auto check = [](char& a,char& b)->bool
+                {
+                    return abs(a - b) == 32;
+                };
+        int n = s.length();
+        for(int i = 0;i<n;i++){
+            if(st.empty()){
+                st.push(s[i]);
+                continue;
+            }else{
+                char a = st.top();
+                char b = s[i];
+                if(check(a,b))
+                {
+                    st.pop();
+                }else{
+                    st.push(s[i]);
+                }
+            }
+            
+        }
+        //ans;
+        n = st.size();
+        s.resize(n);
+        for(int i = 0;i<n;i++){
+            s[n-1 - i] = st.top();
+            st.pop();
+        }
+        return s;
+    }
+};
