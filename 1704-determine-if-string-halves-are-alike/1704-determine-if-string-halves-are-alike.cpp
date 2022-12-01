a7ab2a1815aadd95c347ed46dfa13144496ae544
Arnick-s <96995811+Arnick-s@users.noreply.github.com>
2022-12-01 10:09:57 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (56.35%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -5,15 +5,13 @@ public:
 
         int cnt = 0;
         for(int i = 0;i<n;i++){
-            if(i<n/2){
-                char temp = tolower(s[i]);
-                if(temp == 'a' || temp == 'e' || temp == 'i' || temp == 'o' || temp == 'u')
-                    cnt++;
-            }else{
-                char temp = tolower(s[i]);
-                if(temp == 'a' || temp == 'e' || temp == 'i' || temp == 'o' || temp == 'u')
-                cnt--;
-            }
+            
+            char temp = tolower(s[i]);
+            if(!(temp == 'a' || temp == 'e' || temp == 'i' || temp == 'o' || temp == 'u'))
+                continue;
+            
+            i<n/2 ? cnt++:cnt--;   
+            
         }
         return cnt==0 ? true: false;
     }
